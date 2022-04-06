72f4364fce6b55d1888626bf7fcf000c1e32dcf8
unknown <pjohnny0508@gmail.com>
2022-04-06 03:46:18 -0400

    Add download feature

--- drive/urls.py
+++ drive/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='drive-home'),
+    path('<path:str>', views.download, name='drive-download'),
 ]
--- drive/views.py
+++ drive/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import FileResponse, HttpResponseNotFound
+import glob
+import os
 
 # Create your views here.
-files = [
-    {
-        'name': 'STAT 3559'
-    },
-    {
-        'name': "COMP 4900"
-    },
-    {
-        'name': "MATH 3806"
-    }
-]
 def home(request):
+    files = glob.glob('C:\world\**\*', recursive=True)
+    files = [e[3:] for e in files]
     context = {
         'files': files
     }
     return render(request, 'drive/home.html', context)
+
+def download(request,str):
+    str = os.path.join('C:/', str)
+    if os.path.isdir(str):
+        return HttpResponseNotFound("<h1>404 ERROR FILE NOT FOUND</h1>")
+    f = open(str, "rb")
+    return FileResponse(f)
