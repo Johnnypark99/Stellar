4609d0fefbc185aceaf563c79e3a1b35fcda2abc
unknown <pjohnny0508@gmail.com>
2022-04-08 13:23:48 -0400

    Update to access folders as well

--- drive/views.py
+++ drive/views.py
@@ -5,16 +5,41 @@ import os
 
 # Create your views here.
 def home(request):
-    files = glob.glob('C:\world\**\*', recursive=True)
-    files = [e[3:] for e in files]
+    print("IP Accessing: ", get_client_ip(request))
+    data = glob.glob('C:\\world\\*')
+    files = []
+    for f in data:
+        files.append(f)
     context = {
         'files': files
     }
     return render(request, 'drive/home.html', context)
 
 def download(request,str):
-    str = os.path.join('C:/', str)
-    if os.path.isdir(str):
+    print("IP Accessing: ", get_client_ip(request))
+    if 'C:/world' not in str:
         return HttpResponseNotFound("<h1>404 ERROR FILE NOT FOUND</h1>")
-    f = open(str, "rb")
+    str = os.path.join('C:\\', str)
+    if os.path.isdir(str):
+        files = []
+        data = glob.glob(str+'\*')
+        for f in data:
+            f = os.path.join('C:\\',os.path.normpath(f))
+            files.append(f)
+            context = {
+                'files': files
+            }
+        return render(request, 'drive/home.html', context)
+    try:
+        f = open(str, "rb")
+    except:
+        return HttpResponseNotFound("<h1>404 ERROR FILE NOT FOUND</h1>")
     return FileResponse(f)
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
