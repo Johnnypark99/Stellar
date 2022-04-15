6813cf3073ea49e23ab1acea4c15a3328e4bfb85
unknown <pjohnny0508@gmail.com>
2022-04-15 11:07:35 -0400

    Clean up file path

--- drive/urls.py
+++ drive/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', views.home, name='drive-home'),
+    path('favicon.ico', RedirectView.as_view(url='/static/drive/favicon.ico'), name='drive-fav-icon'),
     path('<path:str>', views.download, name='drive-download'),
 ]
--- drive/views.py
+++ drive/views.py
@@ -1,30 +1,35 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
 from django.http import FileResponse, HttpResponseNotFound
 import glob
 import os
 
 # Create your views here.
+
+
 def home(request):
     print("IP Accessing: ", get_client_ip(request))
     data = glob.glob('C:\\world\\*')
     files = []
     for f in data:
+        f = f[9:]
         files.append(f)
     context = {
         'files': files
     }
     return render(request, 'drive/home.html', context)
 
-def download(request,str):
+
+def download(request, str):
     print("IP Accessing: ", get_client_ip(request))
-    if 'C:/world' not in str:
-        return HttpResponseNotFound("<h1>404 ERROR FILE NOT FOUND</h1>")
-    str = os.path.join('C:\\', str)
+    str = os.path.join('C:\\world\\', str)
     if os.path.isdir(str):
         files = []
         data = glob.glob(str+'\*')
         for f in data:
-            f = os.path.join('C:\\',os.path.normpath(f))
+            print(f)
+            f = os.path.join(os.path.normpath(f))
+            f = f[9:]
             files.append(f)
             context = {
                 'files': files
@@ -35,6 +40,7 @@ def download(request,str):
     except:
         return HttpResponseNotFound("<h1>404 ERROR FILE NOT FOUND</h1>")
     return FileResponse(f)
+
 
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
